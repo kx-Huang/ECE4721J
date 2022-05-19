6a999bfc8697b5df4820fd3a84bf9e83182263ea
kx-huang <Huang.KX@sjtu.edu.cn>
2022-05-19 17:59:17 -0400

    feat: parser and main process

--- lab/lab_1/lab_1_code/src/main/java/com/ve472/l1/Main.java
+++ lab/lab_1/lab_1_code/src/main/java/com/ve472/l1/Main.java
@@ -3,32 +3,56 @@ package com.ve472.l1;
 import org.apache.commons.cli.*;
 
 public class Main {
+    private static CommandLine cmd;
+    private static final Options options = new Options();
+    private static HelpFormatter formatter = new HelpFormatter();
+
     public static void main(String[] args) {
+        handleCommandLineInput(args);
 
-        // command line options
-        Options options = new Options();
-        options.addOption("h", "help", false, "print this message");
-        options.addOption(null, "hall", true, "path of the hall config directory");
-        options.addOption(null, "query", true, "query of customer orders");
-
-        // command line parser
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd;
-        try {
-            cmd = parser.parse(options, args);
-            // print the help message
-            if (cmd.hasOption("h")) {
-                HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp("cinema", options, false);
-            }
-            // get the value of the hall config directory
-            String hallPath = cmd.getOptionValue("hall");
-            if (hallPath != null) {
-                // TODO: parses the configuration files
-            }
-        } catch (ParseException e) {
-            e.printStackTrace();
+        // print the help message
+        if (cmd.hasOption("help")) {
+            formatter.printHelp("cinema", options, false);
+            return;
         }
 
+        // get command line input
+        String cinemaConfigDir = cmd.getOptionValue("hall");
+        String inputQueryFile = cmd.getOptionValue("query");
+        new Cinema(cinemaConfigDir, inputQueryFile);
+    }
+
+    private static void handleCommandLineInput(String[] args) {
+        // command line options
+        options.addOption(Option.builder("h")
+                            .longOpt("help")
+                            .desc("print this message")
+                            .build()
+                         );
+        options.addOption(Option.builder()
+                            .longOpt("hall")
+                            .required()
+                            .hasArg()
+                            .argName("arg")
+                            .desc("path of the hall config directory")
+                            .build()
+                         );
+        options.addOption(Option.builder()
+                            .longOpt("query")
+                            .required()
+                            .hasArg()
+                            .argName("arg")
+                            .desc("query of customer orders")
+                            .build()
+                         );
+
+        // parse command line input
+        CommandLineParser parser = new DefaultParser();
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            formatter.printHelp("cinema", options, false);
+            System.exit(0);
+        }
     }
 }
