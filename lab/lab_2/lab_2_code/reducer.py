944d61bf3030084efb786d7b72a72d8b49232be3
kx-huang <Huang.KX@sjtu.edu.cn>
2022-06-03 19:36:56 -0400

    update: example

--- lab/lab_2/lab_2_code/reducer.py
+++ lab/lab_2/lab_2_code/reducer.py
@@ -3,10 +3,13 @@
 
 # Reads pairs from the standard input. Each tab-separated pair is composed of a studentID and a list of grades
 # Returns the max grade for each student on the standard output.
-# Usage:
-#    STDIN: StudentID\tGrade1, StudentID\tGrade2, ...
+#
+# Input:
+#    StudentID<TAB>Grade1
+#    StudentID<TAB>Grade2
+#    ...
 # Output:
-#    StudentID MaxGrade
+#    StudentID Max(Grade1, Grade2, ...)
 
 
 import sys
