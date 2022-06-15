84ab77dc3d43bf28885652be83985f413f7683cf
kx-huang <Huang.KX@sjtu.edu.cn>
2022-06-15 16:48:17 -0400

    update: benchmark plot script

--- homework/hw_3/hw_3_code/ex1/benchmark/plot.py
+++ homework/hw_3/hw_3_code/ex1/benchmark/plot.py
@@ -8,18 +8,30 @@ os.mkdir(BASE_DIR) if not os.path.exists(BASE_DIR) else None
 START_EXPONENT = 3
 STOP_EXPONENT = 8
 
-############################### Java ###############################
 student_num = [10**i for i in range(START_EXPONENT, STOP_EXPONENT+1)]
 
-total_time_1 = [28, 29, 26, 28, 38, 55]
-total_time_2 = [27, 26, 25, 30, 33, 45]
-total_time_3 = [27, 24, 26, 31, 33, 40]
+total_time_single = [4.088, 4.826, 5.189, 8.904, 55.917, 534]
 
-total_time = []
-for i in range(len(total_time_1)):
-    total_time.append((total_time_1[i] + total_time_2[i] + total_time_3[i])/3)
+total_time_java_1 = [28, 29, 26, 28, 38, 55]
+total_time_java_2 = [27, 26, 25, 30, 33, 45]
+total_time_java_3 = [27, 24, 26, 31, 33, 40]
 
-coeffs = np.polyfit(np.log(student_num), np.log(total_time), deg=4)
+total_time_streaming_1 = [25, 21, 21, 24, 47, 107]
+total_time_streaming_2 = [21, 20, 22, 24, 40, 96]
+total_time_streaming_3 = [22, 20, 23, 22, 45, 95]
+
+total_time_java = []
+for i in range(len(total_time_java_1)):
+    total_time_java.append(
+        (total_time_java_1[i] + total_time_java_2[i] + total_time_java_3[i])/3)
+
+total_time_streaming = []
+for i in range(len(total_time_streaming_1)):
+    total_time_streaming.append(
+        (total_time_streaming_1[i] + total_time_streaming_2[i] + total_time_streaming_3[i])/3)
+
+############################### Java ###############################
+coeffs = np.polyfit(np.log(student_num), np.log(total_time_java), deg=4)
 poly = np.poly1d(coeffs)
 
 x = np.logspace(START_EXPONENT, 1.01*STOP_EXPONENT, 100)
@@ -27,7 +39,7 @@ y = np.exp(poly(np.log(x)))
 
 # Total Time v.s. Number of Students
 plt.plot(x, y, 'orange')
-plt.scatter(student_num, total_time)
+plt.scatter(student_num, total_time_java)
 
 plt.xscale('log')
 plt.xlabel('Number of Records')
@@ -39,21 +51,6 @@ plt.savefig(os.path.join(BASE_DIR, 'cluster-java.png'))
 plt.clf()
 
 ######################## Java v.s. Streaming ########################
-student_num = [10**i for i in range(START_EXPONENT, STOP_EXPONENT+1)]
-
-total_time_1 = [25, 21, 21, 24, 47, 107]
-total_time_2 = [21, 20, 22, 24, 40, 96]
-total_time_3 = [22, 20, 23, 22, 45, 95]
-
-total_time_single = [4.088, 4.826, 5.189, 8.904, 55.917, 534]
-
-total_time_streaming = []
-for i in range(len(total_time_1)):
-    total_time_streaming.append(
-        (total_time_1[i] + total_time_2[i] + total_time_3[i])/3)
-
-total_time_java = total_time
-
 coeffs_java = np.polyfit(
     np.log(student_num), np.log(total_time_java), deg=3)
 coeffs_streaming = np.polyfit(
