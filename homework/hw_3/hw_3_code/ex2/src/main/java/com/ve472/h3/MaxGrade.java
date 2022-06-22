9b93585fc950cd2fd704ceaed2ef428f8b466903
kx-huang <Huang.KX@sjtu.edu.cn>
2022-06-23 02:06:11 +0800

    feat: avro mapreduce code

--- homework/hw_3/hw_3_code/ex2/src/main/java/com/ve472/h3/MaxGrade.java
+++ homework/hw_3/hw_3_code/ex2/src/main/java/com/ve472/h3/MaxGrade.java
@@ -4,25 +4,52 @@ import avro.AvroFile;
 
 import java.io.IOException;
 
-import org.apache.avro.*;
-import org.apache.avro.Schema.Type;
-import org.apache.avro.mapred.*;
-import org.apache.hadoop.conf.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.avro.Schema;
+import org.apache.avro.mapred.AvroKey;
+import org.apache.avro.mapred.AvroValue;
+import org.apache.avro.mapreduce.AvroJob;
+import org.apache.avro.mapreduce.AvroKeyInputFormat;
+import org.apache.avro.mapreduce.AvroKeyValueOutputFormat;
+
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class MaxGrade extends Configured implements Tool {
   public static class MaxGradeMapper
-      extends AvroMapper<AvroKey<AvroFile>, Pair<CharSequence, Integer>> {
-
+      extends Mapper<AvroKey<AvroFile>, NullWritable, Text, IntWritable> {
+    // output key-value pair: studentID-grade
     private Text studentID = new Text();
-    private Integer grade = new Integer(0);
+    private IntWritable grade = new IntWritable(0);
+
+    // decode avro file raw data
+    private String decodeByte(ByteBuffer byteBuffer) throws CharacterCodingException {
+      Charset charset = StandardCharsets.UTF_8;
+      CharsetDecoder charsetDecoder = charset.newDecoder();
+      CharBuffer charBuffer = charsetDecoder.decode(byteBuffer.asReadOnlyBuffer());
+      return charBuffer.toString();
+    }
 
     @Override
-    public void map(AvroKey<AvroFile> key, AvroCollector<Pair<CharSequence, Integer>> collector,
-        Reporter reporter) throws IOException {
-      String raw = key.datum().getFilecontent().array();
+    public void map(AvroKey<AvroFile> key, NullWritable value, Context context)
+        throws IOException, InterruptedException {
+      String raw = decodeByte(key.datum().getFilecontent());
       String[] records = raw.split("\n");
       // split each line by comma
       for (String row : records) {
@@ -34,56 +61,60 @@ public class MaxGrade extends Configured implements Tool {
         studentID.set(cols[1]);
         grade.set(Integer.parseInt(cols[2]));
         // submit output key-value pair
-        collector.collect(new Pair<CharSequence, Integer>(studentID, grade));
+        context.write(studentID, grade);
       }
     }
   }
 
   public static class MaxGradeReducer
-      extends AvroReducer<CharSequence, Integer, Pair<CharSequence, Integer>> {
+      extends Reducer<Text, IntWritable, AvroKey<CharSequence>, AvroValue<Integer>> {
+    private int maxGrade = 0;
+
     @Override
-    public void reduce(CharSequence key, Iterable<Integer> values,
-        AvroCollector<Pair<CharSequence, Integer>> collector, Reporter reporter)
-        throws IOException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+        throws IOException, InterruptedException {
       // get the max grade
-      Integer maxGrade = new Integer(0);
+      int maxGrade = 0;
       while (values.iterator().hasNext())
         maxGrade = Math.max(maxGrade, values.iterator().next().get());
       // generate the output studentID-maxGrade pair.
-      collector.collect(new Pair<CharSequence, Integer>(key, maxGrade));
+      context.write(new AvroKey<CharSequence>(key.toString()), new AvroValue<Integer>(maxGrade));
     }
   }
 
   public int run(String[] args) throws Exception {
-    if (args.length != 2) {
-      System.err.println("Usage: MaxGrade <input path> <output path>");
-      return -1;
-    }
-
     // initialize the mapreduce job
-    JobConf conf = new JobConf(getConf(), MaxGrade.class);
-    conf.setJobName("get max grade");
+    Job job = new Job(getConf());
+    job.setJarByClass(MaxGrade.class);
+    job.setJobName("get max grade");
 
     // set input/output path
-    FileInputFormat.setInputPaths(conf, new Path(args[0]));
-    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    // set the input/output format
+    job.setInputFormatClass(AvroKeyInputFormat.class);
+    job.setOutputFormatClass(AvroKeyValueOutputFormat.class);
+
+    // set the output key/value class
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
 
     // set the mapper/reducer classes
-    AvroJob.setMapperClass(conf, MaxGradeMapper.class);
-    AvroJob.setReducerClass(conf, MaxGradeReducer.class);
+    job.setMapperClass(MaxGradeMapper.class);
+    job.setReducerClass(MaxGradeReducer.class);
 
     // set Avro input/output schema
-    AvroJob.setInputSchema(conf, AvroKey<AvroFile>.getClassSchema());
-    AvroJob.setOutputSchema(
-        conf, Pair.getPairSchema(Schema.create(Type.STRING), Schema.create(Type.INT)));
+    AvroJob.setInputKeySchema(job, AvroFile.getClassSchema());
+    AvroJob.setOutputKeySchema(job, Schema.create(Schema.Type.STRING));
+    AvroJob.setOutputValueSchema(job, Schema.create(Schema.Type.INT));
 
-    // run the mapreduce job
-    JobClient.runJob(conf);
-    return 0;
+    // run the mapreduce job and wait for completion
+    return (job.waitForCompletion(true) ? 0 : 1);
   }
 
   public static void main(String[] args) throws Exception {
-    int res = ToolRunner.run(new Configuration(), new MaxGrade(), args);
+    int res = ToolRunner.run(new MaxGrade(), args);
     System.exit(res);
   }
 }
