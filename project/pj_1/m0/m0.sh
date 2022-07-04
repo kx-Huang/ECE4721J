44e17c8fca9a468ca4ffaefc0876e17aefe8e968
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-04 21:39:15 +0800

    update: m0 shell script

--- project/pj_1/m0/m0.sh
+++ project/pj_1/m0/m0.sh
@@ -4,7 +4,7 @@ mkdir -p data/
 rm -f data/compact.h5
 
 # compact hdf5 files to an aggregated file
-python3 src/create_aggregate_file.py MillionSongSubset/A/A/A/ data/compact.h5
+python3 src/create_aggregate_file.py data/single/ data/compact.h5
 
 # convert hdf5 file to Avro file
 python3 src/hdf5_to_avro.py -s schema/songs.avsc -i data/compact.h5 -o data/output.avro
