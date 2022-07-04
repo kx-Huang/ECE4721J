042028ae61d287b19e17ec2ffc58d025a8bf36ec
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-04 19:19:12 +0800

    rename: m0 sciprt

--- project/pj_1/m0/compact.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p data/
-rm -f data/compact.h5
-python3 src/create_aggregate_file.py ../MillionSongSubset/A/A/A/ data/compact.h5
-python3 src/display_song.py data/compact.h5 5 artist_name
