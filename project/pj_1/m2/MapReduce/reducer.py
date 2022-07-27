a49eddd7cdba20f2a0b0cabf0ac20b84f55e72ac
kx-huang <Huang.KX@sjtu.edu.cn>
2022-07-27 18:55:39 +0800

    add: reducer comment

--- project/pj_1/m2/MapReduce/reducer.py
+++ project/pj_1/m2/MapReduce/reducer.py
@@ -13,12 +13,16 @@ for line in sys.stdin:
     line = line.strip()
     # parse the input we got from mapper.py
     node, parent, dist = line.split(',')
+
     # convert dist (currently a string) to int
     try:
         dist = int(dist)
     except ValueError:
         # dist was not a number, so silently ignore/discard this line
         continue
+
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: node) before it is passed to the reducer
     if current_node == node:
         if current_dist > dist:
             current_dist = dist
@@ -32,5 +36,7 @@ for line in sys.stdin:
         current_dist = dist
         current_node = node
         neighbours = parent
+
+# do not forget to output the last node if needed!
 if current_node == node:
     print('%s|%s|%s' % (current_node, str(current_dist), neighbours))
